9f6790728dd128ff375860eabac440c033be5cc7
leonard <leo.decaillet@d-web.ch>
2024-12-23 08:54:11 +0100

    v9.3.2 Revue inscription automatique anniversaire

--- models/ClientsOnline.php
+++ models/ClientsOnline.php
@@ -118,6 +118,21 @@ class ClientsOnline extends \yii\db\ActiveRecord
             11 => true,
             12 => true,
             '12+' => false,
+        ],
+        '12+-aventure-240' => [
+            1 => false,
+            2 => false,
+            3 => false,
+            4 => false,
+            5 => false,
+            6 => false,
+            7 => false,
+            8 => false,
+            9 => false,
+            10 => false,
+            11 => false,
+            12 => false,
+            '12+' => false,
         ]
     ];
     
